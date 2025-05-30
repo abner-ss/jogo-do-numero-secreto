65b31cdf22d4721b3f41225e4fc73a703c866c90
Abner Santos Soares <abnersantos.soares@gmail.com>
2025-05-30 16:47:38 -0300

    projeto inicial

---
+++ js-curso-2-aula_2/app.js
@@ -0,0 +1,15 @@
+let nota1 = 7;
+let nota2 = 6;
+let nota3 = 3;
+let nota4 = 5;
+
+function calcularMedia(nota1, nota2, nota3, nota4) {
+    let media = (nota1 + nota2 + nota3 + nota4) / 4;
+    return media
+}
+
+function verificarAprovacao(media) {
+    return resultado >=5 ? 'Aprovado' : 'Reprovado';
+}
+
+console.log(`Média: ${media} - ${verificarAprovacao(resultado)}`);
