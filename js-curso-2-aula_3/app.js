68905609447d248ad0aeeef54f58dc3e070733b8
Abner Santos Soares <abnersantos.soares@gmail.com>
2025-06-04 14:25:40 -0300

    Corrigindo sintaxe da mensagem de acerto

--- js-curso-2-aula_3/app.js
+++ js-curso-2-aula_3/app.js
@@ -17,7 +17,7 @@ function verificarChute() {
     let chute = document.querySelector('input').value;
     
     if (chute == numeroSecreto) {
-        exibirTextoNaTela('h1', 'Isso aí, Acertou!');
+        exibirTextoNaTela('h1', 'Isso aí, acertou!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
